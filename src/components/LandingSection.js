5d9dc92bd40816850507913339bcd1a818d12535
Ahmad Piracha <AhmadPiracha@users.noreply.github.com>
2023-07-18 02:09:39 +0500

    Landing Page Completed

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Ahmad Piracha";
 const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio2 = "specialized in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +15,22 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4}>
+      <Avatar
+        size="2xl"
+        name="Ahmad Piracha"
+        src="https://i.pravatar.cc/150?img=7"
+      />
+      <Heading size="2xl" color="white">
+        {greeting}
+      </Heading>
+      <Heading size="lg" color="white">
+        {bio1}
+      </Heading>
+      <Heading size="lg" color="white">
+        {bio2}
+      </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
